0b30dea50effde4be9454f5c76d03e4ba1df734d
atikdev-bd <atikdev.bd@gmail.com>
2022-09-03 02:51:01 +0600

    blog icon author photo add

--- script.js
+++ script.js
@@ -24,24 +24,39 @@ const countCategories = (id) => {
 };
 const newsCategories = (data) => {
   const displayDiv = document.getElementById("blogs");
-  displayDiv.innerHTML = ''
+  displayDiv.innerHTML = "";
   data.forEach((news) => {
-    console.log(news)
+    console.log(news);
     const div = document.createElement("div");
-    div.classList.add('news-blogs')
+    div.classList.add("news-blogs");
     div.innerHTML = `
                 <div class="card card-side bg-base-100 shadow-xl mx-16">
                    <figure><img src="${news.thumbnail_url}" alt=""></figure>
                    <div class="card-body">
-                      <h2 class="card-title">New movie is released!</h2>
-                       <p>Click the button to watch on Jetflix app.</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Watch</button>
-                      </div>
-                  </div>
+                        <h1 class="card-title text-2xl">${news.title}</h1>
+                        <p>${news.details.slice(0, 200) + "....."}</p>
+                         <div class="card-actions justify-end">
+                             <button class="btn btn-primary">Watch</button>
+                           </div>
+                          <div class="flex">
+                               <img class="w-14 rounded-full" src="${news.author.img}" />
+                                <div class="ml-5">
+                                 <p> ${news.author.name}</p>
+                                 <p> ${news.author.published_date}</p>
+                               </div>
+                               <span class="text-center ml-28 mt-6"><i class="fa-regular fa-eye"></i> ${news.total_view}</span>
+                               <span class="text-center ml-28 mt-6">
+                               <i class="fa-solid fa-star"></i>
+                               <i class="fa-regular fa-star"></i>
+                               <i class="fa-regular fa-star"></i>
+                               <i class="fa-regular fa-star"></i>
+                               <i class="fa-regular fa-star"></i>
+                               </span>
+                          </div>
+                     </div>
                 </div>
            `;
-           displayDiv.appendChild(div)
+    displayDiv.appendChild(div);
   });
 };
 
