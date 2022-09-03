cdbdad4ceb245ccc78d66d9051699d909d634871
atikdev-bd <atikdev.bd@gmail.com>
2022-09-03 14:00:56 +0600

    blog click question ans add

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ const displayCategories = (categories) => {
   categories.forEach((categorie) => {
     const div = document.createElement("div");
     div.innerHTML = `
-    <button onclick="countCategories('${categorie.category_id}')" class="hover:text-orange-200">${categorie.category_name}</button>
+    <button onclick="countCategories('${categorie.category_id ? categorie.category_id : 'DATA NOT FOUND'}')" class="hover:text-orange-200">${categorie.category_name}</button>
     `;
     displayDiv.appendChild(div);
   });
 };
+/// SPPINER SECTION ////
+const toggleSpiner = (isLodding) => {
+    const spiner = document.getElementById('lodding');
+    if (isLodding) {
+        spiner.classList.remove('d-none')
+    }
+    else {
+        spiner.classList.add('d-none')
+    }
+}
 
 const countCategories = (id) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
@@ -35,12 +45,16 @@ const newsCategories = (data) => {
                         <h1 class="card-title text-2xl">${news.title}</h1>
                         <p>${news.details.slice(0, 200) + "....."}</p>
                           <div class="flex">
-                               <img class="w-14 rounded-full" src="${news.author.img}" />
+                               <img class="w-14 rounded-full" src="${
+                                 news.author.img
+                               }" />
                                 <div class="ml-5">
-                                 <p> ${news.author.name}</p>
-                                 <p> ${news.author.published_date}</p>
+                                 <p> ${news.author.name ? news.author.name : 'N/A' }</p>
+                                 <p> ${news.author.published_date ? news.author.published_date : 'N/A'}</p>
                                </div>
-                               <span class="text-center ml-28 mt-6"><i class="fa-regular fa-eye"></i> ${news.total_view}</span>
+                               <span class="text-center ml-28 mt-6"><i class="fa-regular fa-eye"></i> ${
+                                 news.total_view
+                               }</span>
                                <span class="text-center ml-28 mt-6">
                                <i class="fa-solid fa-star"></i>
                                <i class="fa-regular fa-star"></i>
@@ -48,7 +62,8 @@ const newsCategories = (data) => {
                                <i class="fa-regular fa-star"></i>
                                <i class="fa-regular fa-star"></i>
                                </span>
-                               <span class="ml-72"><button class="btn glass">Details</button></span>
+                               
+                               <span class="ml-72"><label onclick="idApi('${news._id}')" for="my-modal-3" class="btn glass modal-button">Details</label></span>
                             </div>
                     </div>
                           
@@ -58,5 +73,36 @@ const newsCategories = (data) => {
     displayDiv.appendChild(div);
   });
 };
+
+const idApi = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => modalDisplay(data.data));
+};
+
+const modalDisplay = (datas)=>{
+    const modalDiv = document.getElementById('modal-id')
+    datas.forEach(data =>{
+        console.log(data)
+
+    modalDiv.innerHTML = `
+    <label
+            for="my-modal-3"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+            >✕</label
+          >
+          <h3 class="text-lg font-bold"> Author Name : ${data.author.name? data.author.name : 'Data not available'}</h3>
+           <p class="py-4">Total View : ${data.total_view? data.total_view : 'Data not available'}</p>
+          <p class="py-4">Published Date : ${data.author.published_date? data.author.published_date :'Data not available'}</p>
+    
+    `
+    
+    })
+
+
+}
+
+
 
 newsApi();
