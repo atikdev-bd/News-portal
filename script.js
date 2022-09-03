7f9bb1a9bbca68532ef2307dd086669a656d60cf
atikdev-bd <atikdev.bd@gmail.com>
2022-09-03 16:53:54 +0600

    loading spinner add

--- script.js
+++ script.js
@@ -2,40 +2,50 @@ const newsApi = () => {
   const url = "https://openapi.programming-hero.com/api/news/categories";
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayCategories(data.data.news_category));
+    .then((data) => displayCategories(data.data.news_category))
+    .catch((error) => console.log(error));
 };
 
 const displayCategories = (categories) => {
+  
   const displayDiv = document.getElementById("categories");
   categories.forEach((categorie) => {
     const div = document.createElement("div");
     div.innerHTML = `
-    <button onclick="countCategories('${categorie.category_id ? categorie.category_id : 'DATA NOT FOUND'}')" class="hover:text-orange-200">${categorie.category_name}</button>
+    <button onclick="countCategories('${
+      categorie.category_id ? categorie.category_id : "DATA NOT FOUND"
+    }')" class="hover:text-orange-200 hover:bg-slate-600 rounded px-2">${
+      categorie.category_name
+    }</button>
     `;
     displayDiv.appendChild(div);
   });
 };
-/// SPPINER SECTION ////
-const toggleSpiner = (isLodding) => {
-    const spiner = document.getElementById('lodding');
-    if (isLodding) {
-        spiner.classList.remove('d-none')
-    }
-    else {
-        spiner.classList.add('d-none')
-    }
-}
+
+
 
 const countCategories = (id) => {
+  toggleSpiner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => newsCategories(data.data));
+    .then((data) => newsCategories(data.data))
+    .catch((error) => console.log(error));
 };
 const newsCategories = (data) => {
+  const countCategories = document.getElementById('text-id');
+  if(data.length === 0){
+  countCategories.innerText = data.length + ' ' +'items found for this category'
+  toggleSpiner(false);
+  }
+  else{
+    countCategories.innerText = data.length + ' ' +'items found for this category'
+  }
+  
   const displayDiv = document.getElementById("blogs");
   displayDiv.innerHTML = "";
   data.forEach((news) => {
+    
     const div = document.createElement("div");
     div.classList.add("news-blogs");
     div.innerHTML = `
@@ -49,11 +59,17 @@ const newsCategories = (data) => {
                                  news.author.img
                                }" />
                                 <div class="ml-5">
-                                 <p> ${news.author.name ? news.author.name : 'N/A' }</p>
-                                 <p> ${news.author.published_date ? news.author.published_date : 'N/A'}</p>
+                                 <p> ${
+                                   news.author.name ? news.author.name : "N/A"
+                                 }</p>
+                                 <p> ${
+                                   news.author.published_date
+                                     ? news.author.published_date
+                                     : "N/A"
+                                 }</p>
                                </div>
                                <span class="text-center ml-28 mt-6"><i class="fa-regular fa-eye"></i> ${
-                                 news.total_view
+                                 news.total_view ? news.total_view : "0"
                                }</span>
                                <span class="text-center ml-28 mt-6">
                                <i class="fa-solid fa-star"></i>
@@ -61,30 +77,41 @@ const newsCategories = (data) => {
                                <i class="fa-regular fa-star"></i>
                                <i class="fa-regular fa-star"></i>
                                <i class="fa-regular fa-star"></i>
-                               </span>
-                               
-                               <span class="ml-72"><label onclick="idApi('${news._id}')" for="my-modal-3" class="btn glass modal-button">Details</label></span>
+                               </span>                             
+                               <span class="ml-72"><label onclick="idApi('${
+                                 news._id
+                               }')" for="my-modal-3" class="btn glass modal-button">Details</label></span>
                             </div>
-                    </div>
-                          
+                   </div>             
                 </div>
-            
            `;
     displayDiv.appendChild(div);
+    toggleSpiner(false);
   });
+};
+
+/// SPPINER SECTION ////
+const toggleSpiner = (isLodding) => {
+  const spiner = document.getElementById("toggle");
+  if (isLodding) {
+    spiner.classList.remove("hidden");
+  } else {
+    spiner.classList.add("hidden");
+  }
 };
 
 const idApi = (id) => {
   const url = `https://openapi.programming-hero.com/api/news/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => modalDisplay(data.data));
+    .then((data) => modalDisplay(data.data))
+    .catch((error) => console.log(error));
 };
 
-const modalDisplay = (datas)=>{
-    const modalDiv = document.getElementById('modal-id')
-    datas.forEach(data =>{
-        console.log(data)
+const modalDisplay = (datas) => {
+  const modalDiv = document.getElementById("modal-id");
+  datas.forEach((data) => {
+    console.log(data);
 
     modalDiv.innerHTML = `
     <label
@@ -92,17 +119,19 @@ const modalDisplay = (datas)=>{
             class="btn btn-sm btn-circle absolute right-2 top-2"
             >✕</label
           >
-          <h3 class="text-lg font-bold"> Author Name : ${data.author.name? data.author.name : 'Data not available'}</h3>
-           <p class="py-4">Total View : ${data.total_view? data.total_view : 'Data not available'}</p>
-          <p class="py-4">Published Date : ${data.author.published_date? data.author.published_date :'Data not available'}</p>
-    
-    `
-    
-    })
-
-
-}
-
-
+          <h3 class="text-lg font-bold"> Author Name : ${
+            data.author.name ? data.author.name : "Data not available"
+          }</h3>
+           <p class="py-4">Total View : ${
+             data.total_view ? data.total_view : "Data not available"
+           }</p>
+          <p class="py-4">Published Date : ${
+            data.author.published_date
+              ? data.author.published_date
+              : "Data not available"
+          }</p>
+    `;
+  });
+};
 
 newsApi();
